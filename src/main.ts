a136b51e1504bd01b714aa9b6e3e80dc6da68d1b
Rain.qin-vvbdc <kuguago@gmail.com>
2024-07-04 16:13:36 +0800

    feat: 添加返回格式format和异常状态捕获器

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filter/http-exception/http-exception.filter';
+import { TransformInterceptor } from './common/interceptor/transform/transform.interceptor';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 
   if (module.hot) {
