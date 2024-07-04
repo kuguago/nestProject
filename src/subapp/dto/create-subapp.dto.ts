a95d2a1eae1a5458347171355667457cdf4a85e1
Rain.qin-vvbdc <kuguago@gmail.com>
2024-07-04 16:43:37 +0800

    新增env.prod文件，删除无用代码

--- src/subapp/dto/create-subapp.dto.ts
+++
@@ -1,6 +0,0 @@
-import { VersionDto } from '../../version/dto/create-version.dto';
-
-export class CreateSubappDto {
-  subappName: string;
-  versions?: VersionDto[];
-}
--- src/subapp/dto/update-subapp.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSubappDto } from './create-subapp.dto';
-
-export class UpdateSubappDto extends PartialType(CreateSubappDto) {}
